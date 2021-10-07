752ff66909f1ae83e2a14e62c8b4eaec1b94aa87
Tanveer Shah <tanvirhussein@gmail.com>
2021-10-06 23:02:57 -0400

    EER HANDLING: error handler for single post route  added

--- app.js
+++ app.js
@@ -1,12 +1,101 @@
 const express = require("express");
 const morgan = require("morgan");
+const { list, find } = require("./postData");
+const path = require("path");
+
 const app = express();
 
-app.use(morgan('dev'))
+const staticMiddleware = express.static(path.join(__dirname, "public"));
+
+app.use(staticMiddleware);
+app.use(morgan("dev"));
 
 // GET /
 app.get("/", (req, res) => {
-  res.send("Hello World!!");
+  const posts = list();
+
+  const htmlDoc = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts
+        .map(
+          (post) => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲ </span><a href="/posts/${post.id}">${post.title}</a>
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`
+        )
+        .join("")}
+    </div>
+  </body>
+</html>`;
+
+  res.send(htmlDoc);
+});
+
+// GET /posts/:id
+app.get("/posts/:id", (req, res) => {
+  const postId = req.params.id;
+  const post = find(postId);
+  if (!post.id) {
+    res.status(404);
+
+    const htmlDoc = `
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <header><img src="/logo.png"/>Wizard News</header>
+    <div class="not-found">
+      <p>Accio Page! 🧙‍♀️ ... Page Not Found</p>
+      <img src="/dumbledore-404.gif" />
+    </div>
+  </body>
+  </html>`;
+
+    res.send(htmlDoc);
+  } else {
+    const htmlDoc = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲ </span>${post.title}
+            <small>(by ${post.name})</small>
+          </p>
+          <div><p>${post.content}</P></div>
+          <small class="news-info">
+            ${post.date}
+          </small>
+        </div>
+   
+    </div>
+  </body>
+</html>`;
+
+    res.send(htmlDoc);
+  }
 });
 
 const PORT = 1337;
